93f501f18667d82c0d62d115d829cefba24c3881
Raiya Minhas <raiya.minhas@gmail.com>
2026-05-17 11:43:41 -0400

    matrix.h design

---
+++ include/matrix.h
@@ -0,0 +1,27 @@
+#pragma once 
+
+#include <vector>
+#include <stdexcept> 
+
+template <typename T> 
+
+class Matrix {
+public: 
+    int rows;
+    int cols; 
+    std::vector<T> data; 
+
+Matrix(int rows, int cols) {
+    rows = rows;
+    cols = cols;
+    data = rows*cols;
+}
+
+T& operator(int row, int col) {
+    return data[rows*cols + col];
+}
+
+T& operator(int row, int col) {
+    return data[row * cols + col];
+}
+};
